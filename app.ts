a680ebbecd1921030eb3f78ab18d9a7c6bbb30c0
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-04-02 23:35:59 +0900

    타입스크립트의 타입추론

--- app.ts
+++ app.ts
@@ -1,7 +1,22 @@
-function logName(name: string) {
-  console.log(name);
+// Type Inference(타입추론)
+// 예시 1
+let a = 5;
+// a = "hello";
+// 타입스크립트는 변수 a를 숫자(Number)로 추론 하기 때문에 문자(String)형식으로 변경하려하면 에러발생.
+
+//예시 2
+let student = {
+  name: "jack",
+  courseL: "Getting Started with TypeScript",
+  condingIQ: 80,
+  code: function () {
+    console.log("brain is working hard");
+  },
+};
+// student.name = 10; => 타입추론으로 인하여 에러발생
+
+//예시 3
+function calculateCodingIQ(lostPoint) {
+  return 100 - lostPoint;
 }
-
-logName("jack");
-
-console.log("12323");
+// 함수에 마우스를 올리면 function calculateCodingIQ(lostPoint: any): number 과 같이 lostPoint의 형식을 숫자형으로 추론한 것을 알 수있음.
