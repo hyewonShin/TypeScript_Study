0f8af367c829ced196f79f623f5077d1dba362f1
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-04-03 23:55:30 +0900

    타입명시

--- app.js
+++ app.js
@@ -18,3 +18,17 @@ function calculateCodingIQ(lostPoint) {
     return 100 - lostPoint;
 }
 // 함수에 마우스를 올리면 function calculateCodingIQ(lostPoint: any): number 과 같이 lostPoint의 형식을 숫자형으로 추론한 것을 알 수있음.
+// 타입명시
+// : 변수를 선언할 때, 변수 값의 타입을 명시함으로써 변수 값의 데이터 타입을 지정
+var studentID = 12345;
+var studentName = "Jenny Kim";
+var age = 21;
+var gender = "female";
+var subject = "Javascript";
+var courseCompleted = false;
+function getStudentDetails(studentID) {
+    return null;
+}
+// 만일 함수가 특정 값을 반환하는 경우 반환되는 타입을 명시해주면 된다.
+// 아무것도 반환하지 않는다는다면 :void를 명시해준다.
+// void나 any 타입이 아닌 경우에 아무런 값도 반화하지 않는다면 에러가 난다.
